d1dc1d782ea0348e2d9c2e0e970dfa58269a9a15
enoc pineda <enocpinedaa@gmail.com>
2022-01-30 12:43:40 -0600

    Cambiando any por el tipo de Dato

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { SearchGifsResponse, Gif } from '../interface/gif.interface';
 @Injectable({
   providedIn: 'root'
 })
@@ -8,7 +9,7 @@ export class GifsService {
   private _historial: string[] = [];
   private apiKey: string = 'OsQG7IogirXwlJRlUAUYQQjPKE1Ws9fw';
   //cambiar any por su correspondiente
-  public resultados: any =[];
+  public resultados: Gif[] =[];
 
 
   get historial() {
@@ -26,8 +27,8 @@ export class GifsService {
       this._historial = this._historial.splice(0,10);
     }
 
-    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=${this.apiKey}&q=${query}&limit=10`)
-    .subscribe((respuesta : any) => {
+    this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=${this.apiKey}&q=${query}&limit=10`)
+    .subscribe((respuesta : SearchGifsResponse) => {
      this.resultados = respuesta.data; 
     })
   }
